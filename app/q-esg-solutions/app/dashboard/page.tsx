4f338f2939b2c95e22e99da4d29a92b4c6503ad0
agont a <57467835+alex-varga14@users.noreply.github.com>
2024-10-05 02:29:03 -0600

    feat: import relevant shadcn dashboard template

---
+++ app/q-esg-solutions/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { OverViewPageView } from '@/views/overview/view';
+
+export const metadata = {
+  title: 'Dashboard : Overview'
+};
+
+export default function page() {
+  return <OverViewPageView />;
+}
