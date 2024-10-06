69191aa9713675f5870573c1824d21d63fcfa8a5
agont a <57467835+alex-varga14@users.noreply.github.com>
2024-10-06 10:43:09 -0600

    typescript doesnt like customized treee map

--- app/q-esg-solutions/views/results/tree-map-graph.tsx
+++ app/q-esg-solutions/views/results/tree-map-graph.tsx
@@ -188,10 +188,10 @@ export function TreeGraph() {
                     config={chartConfig}
                     className="aspect-auto h-[310px] w-full"
                 >
-                    {/* <ResponsiveContainer width="100%" height="100%">
-                        <Treemap width={400} height={200} data={chartData} dataKey="size" aspectRatio={4 / 3} stroke="#fff" fill="#8884d8" />
-                    </ResponsiveContainer> */}
                     <ResponsiveContainer width="100%" height="100%">
+                        <Treemap width={400} height={200} data={chartData} dataKey="size" aspectRatio={4 / 3} stroke="#fff" fill="#8884d8" />
+                    </ResponsiveContainer>
+                    {/* <ResponsiveContainer width="100%" height="100%">
                         <Treemap
                             width={400}
                             height={200}
@@ -201,7 +201,7 @@ export function TreeGraph() {
                             fill="#8884d8"
                             content={<CustomizedContent colors={COLORS} />}
                         />
-                    </ResponsiveContainer>
+                    </ResponsiveContainer> */}
                 </ChartContainer>
             </CardContent>
             <CardFooter>
